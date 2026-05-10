40d12030a5cee9b17c966ad807b04c3680962176
Neil Anly M. Villanueva <villanueva.neil.anly@gmail.com>
2026-05-10 22:16:34 +0800

    Added: sound effects

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from .auth import auth
 from .main import main
 from .api import api
+from .gacha import gacha
 
 def init_routes(app):
     app.register_blueprint(auth)
     app.register_blueprint(main)
     app.register_blueprint(api)
+    app.register_blueprint(gacha)
